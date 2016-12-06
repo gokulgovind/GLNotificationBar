8ced86c7df91ad124b3e2575dd23c3174b2fb30c
पवन कुमार शर्मा <proactive-solutions@users.noreply.github.com>
2016-12-06 14:55:09 +0530

    Fixed compilation warning

--- Example/GLNotificationBar/ViewController.swift
+++ Example/GLNotificationBar/ViewController.swift
@@ -37,74 +37,74 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func showNotification(sender: AnyObject) {
+    @IBAction func showNotification(_ sender: AnyObject) {
         
         var style:GLNotificationStyle!
         
         if notificationBarType.selectedSegmentIndex == 0 {
-            style = .DetailedBanner
+            style = .detailedBanner
         }else{
-            style = .SimpleBanner
+            style = .simpleBanner
         }
         
-        let notificationBar = GLNotificationBar(title: notificationTitle.text, message:notificationMessage.text , preferredStyle:style) { (bool) in
-            let alert = UIAlertController(title: "Handler", message: "Catch didSelectNotification action in GLNotificationBar completion handler.", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
+        let notificationBar = GLNotificationBar(title: notificationTitle.text ?? "", message:notificationMessage.text ?? "" , preferredStyle:style) { (bool) in
+            let alert = UIAlertController(title: "Handler", message: "Catch didSelectNotification action in GLNotificationBar completion handler.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
-        if notificationAction.on {
-            //Type: .Cancel
-            notificationBar.addAction(GLNotifyAction(title: "Cancel", style: .Cancel, handler: { (result) in
-                let alert = UIAlertController(title: result.actionTitle, message: "Apply a style that indicates the action cancels the operation and leaves things unchanged.", preferredStyle: .Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
+        if notificationAction.isOn {
+            //Type: .cancel
+            notificationBar.addAction(GLNotifyAction(title: "Cancel", style: .cancel, handler: { (result) in
+                let alert = UIAlertController(title: result.actionTitle, message: "Apply a style that indicates the action cancels the operation and leaves things unchanged.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }))
             
             //Type: .Destructive
-            notificationBar.addAction(GLNotifyAction(title: "Destructive", style: .Destructive, handler: { (result) in
-                let alert = UIAlertController(title: result.actionTitle, message: " Apply a style that indicates the action might change or delete data.", preferredStyle: .Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
+            notificationBar.addAction(GLNotifyAction(title: "Destructive", style: .destructive, handler: { (result) in
+                let alert = UIAlertController(title: result.actionTitle, message: " Apply a style that indicates the action might change or delete data.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }))
             
             //Type: .Default
-            notificationBar.addAction(GLNotifyAction(title: "Default", style: .Default, handler: { (result) in
-                let alert = UIAlertController(title: result.actionTitle, message: "Apply the default style to the action’s button.", preferredStyle: .Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
+            notificationBar.addAction(GLNotifyAction(title: "Default", style: .default, handler: { (result) in
+                let alert = UIAlertController(title: result.actionTitle, message: "Apply the default style to the action’s button.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }))
             
-            //Type: .TextInput
-            notificationBar.addAction(GLNotifyAction(title: "Text Input", style: .TextInput, handler: { (result) in
-                let alert = UIAlertController(title: result.actionTitle, message: "Apply a style that indicates the action opens an textinput field helps to respond notification as string.", preferredStyle: .Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
+            //Type: .textInput
+            notificationBar.addAction(GLNotifyAction(title: "Text Input", style: .textInput, handler: { (result) in
+                let alert = UIAlertController(title: result.actionTitle, message: "Apply a style that indicates the action opens an textinput field helps to respond notification as string.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }))
             
             
 //            //Type: .OnlyTextInput
 //            
 //            notificationBar.addAction(GLNotifyAction(title: "Reply", style: .OnlyTextInput, handler: { (result) in
-//                let alert = UIAlertController(title: result.actionTitle, message: " Apply a style which removes all other action added and simply adds text field as input to respond notification.", preferredStyle: .Alert)
-//                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-//                self.presentViewController(alert, animated: true, completion: nil)
+//                let alert = UIAlertController(title: result.actionTitle, message: " Apply a style which removes all other action added and simply adds text field as input to respond notification.", preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
 //            }))
         }
         
         
         notificationBar.showTime(stepper.value)
         
-        if sound.on {
-            notificationBar.notificationSound(soundName.text, ofType: soundType.text, vibrate: vibrate.on)
+        if sound.isOn {
+            notificationBar.notificationSound(soundName.text, ofType: soundType.text, vibrate: vibrate.isOn)
         }
         
     }
     
-    @IBAction func hideKeyboard(sender: UIButton!) {
+    @IBAction func hideKeyboard(_ sender: UIButton!) {
         self.view.endEditing(true)
     }
     
-    @IBAction func timeOutInterval(sender: UIStepper) {
+    @IBAction func timeOutInterval(_ sender: UIStepper) {
         timeOutLabel.text = "Time out interval  \(String(sender.value))"
     }
     
@@ -112,7 +112,7 @@ class ViewController: UIViewController {
 
 
 extension ViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
